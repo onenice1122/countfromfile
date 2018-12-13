20588d52cb15431acf985ac7d75047da23d1a766
JY <123@pccu.com>
2018-12-13 13:56:36 +0800

    second commit

---
+++ src/countfromfile/pccu/com/ScannerCountFromFile.java
@@ -0,0 +1,33 @@
+package countfromfile.pccu.com;
+ import java.io.File;
+ import java.io.FileNotFoundException;
+import java.util.Scanner;
+ public class ScannerCountFromFile {
+ public static void main(String[] args) {
+ Scanner scanner = null;
+ int mSum = 0;
+ int mAmount = 0;
+ String mCustomer = "";
+ try {
+	 
+ scanner = new Scanner(new File("src/consume.txt"));
+ while (scanner.hasNext()) {
+ // handle customer name
+ mCustomer = scanner.next();
+
+ // handle amount
+ mAmount = scanner.nextInt();
+ mSum += mAmount;
+
+ System.out.println("Name: " + mCustomer +
+ " " + "Amount: " +
+ mAmount);
+ }
+ } catch (FileNotFoundException e) {
+ e.printStackTrace();
+ } finally {
+	 scanner.close();
+	  }
+	  System.out.println("Total: " + mSum);
+ }
+ }
